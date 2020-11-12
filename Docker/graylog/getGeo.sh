9cc1ab69235a3768f4c42a044ac8aebcc85a7982
abol4671 <bolde014@umn.edu>
2020-11-12 10:09:44 -0600

    Rpi4 update (#1)
    * Update README.md
    
    * Create time.env
    
    * Update graylog.env
    
    * Create commonDockerCommands.txt
    
    * Update docker-compose.yml
    
    * Update Dockerfile
    
    * Update getGeo.sh
    
    * Update graylog.env

--- Docker/graylog/getGeo.sh
+++ Docker/graylog/getGeo.sh
@@ -1,2 +1,3 @@
-curl --output /etc/graylog/server/mm.tar.gz https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+# Register an account with Maxmind to generate a token.
+curl --output /etc/graylog/server/mm.tar.gz "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=<Maxmind_Token_Here>&suffix=tar.gz"
 tar zxvf /etc/graylog/server/mm.tar.gz -C /etc/graylog/server/ --strip-components=1
